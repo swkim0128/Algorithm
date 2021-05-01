0478ba3cc22320b51095067e5837995e68fc4303
eunsol <eunsol@eunsolui-MacBookAir.local>
2021-05-01 18:26:41 +0900

    stash merge file

--- python/src/main/algorithms/Excute.py
+++ python/src/main/algorithms/Excute.py
@@ -1,9 +1,12 @@
 # from algorithms.dongbinna.greedy02 import solution as sol
 # from algorithms.Baekjoon import solution as sol
-from algorithms.programmers.hash.testKit04 import solution as sol
+from algorithms.programmers.stackqueue.testKit01 import solution as sol
 
 def excute():
+    length = 2
+    weight = 10
+    truck = [7, 4, 5, 6]
 
-    # result = sol(generes, plays)
+    result = sol(length, weight, truck)
 
     print(result)
