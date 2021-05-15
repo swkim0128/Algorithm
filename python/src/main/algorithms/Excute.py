28cab03c1b522a7141912444c0140442695e33e0
swkim0128 <swkim0128@gmail.com>
2021-05-15 20:22:49 +0900

    ENG: programmers stack queue
    프로그래머스 스택/큐 문제 추가

--- python/src/main/algorithms/Excute.py
+++ python/src/main/algorithms/Excute.py
@@ -1,10 +1,11 @@
-# from algorithms.dongbinna import solution as sol
+# from algorithms.dongbinna.greedy03 import solution as sol
 # from algorithms.Baekjoon import solution as sol
-from algorithms.programmers.hash.testKit02 import solution as sol
+from algorithms.programmers.stackqueue.testKit03 import solution as sol
+
 
 def excute():
-    args = ['12', '123', '321']
-    
-    result = sol(args)
+    c = [93, 30, 55]
+    s = [1, 30, 5]
+    result = sol(c, s)
 
     print(result)
