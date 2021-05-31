a75d2fb7b79b90626a8a4bd60a2e0e7c3b018729
swkim0128 <swkim0128@gmail.com>
2021-05-31 18:04:12 +0900

    CLN: modify programmers stackqueue testKit04
    프로그래머스 스택 큐 문제 중 네번째 문제에 대해 추가.
    관련 내용으로 문제 4는 아직 다 풀지 못하였음.
    조금만 더 스택 및 큐를 이용하여 문제를 풀어야함.

--- python/src/main/algorithms/Excute.py
+++ python/src/main/algorithms/Excute.py
@@ -1,11 +1,10 @@
 # from algorithms.dongbinna.greedy03 import solution as sol
 # from algorithms.Baekjoon import solution as sol
-from algorithms.programmers.stackqueue.testKit03 import solution as sol
+from algorithms.programmers.greedy.testKit02 import solution as sol
 
 
 def excute():
-    c = [93, 30, 55]
-    s = [1, 30, 5]
-    result = sol(c, s)
+    name = "JAN"
+    result = sol(name)
 
     print(result)
