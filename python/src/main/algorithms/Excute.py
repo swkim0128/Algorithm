245285b2a4831e917db35ccb591066d0d266d683
swkim0128 <swkim0128@gmail.com>
2021-06-22 16:55:45 +0900

    ENG: modify execute.py
    파이썬 알고리즘 해결과 관련하여 해당 실행 방안에 대해 약간의 개선을 함.
    execute 파일에 랜덤 모듈을 추가하여 10회 실행 후 결과값과 해당 입력값을 화면에 출력하도록 함.
    
    덕분에 유용한 테스트 케이스와 어디서 알고리즘의 취약점이 있는지 알게되었음.

--- python/src/main/algorithms/Excute.py
+++ python/src/main/algorithms/Excute.py
@@ -1,10 +1,15 @@
 # from algorithms.dongbinna.greedy03 import solution as sol
 # from algorithms.Baekjoon import solution as sol
-from algorithms.programmers.greedy.testKit02 import solution as sol
+from algorithms.programmers.greedy.testKit03 import solution as sol
+import random
 
 
 def excute():
-    name = "JAN"
-    result = sol(name)
 
-    print(result)
+    for _ in range(10):
+        number = str(random.randrange(1, 1000000))
+        k = random.randrange(1, len(number))
+
+        result = sol(number, k)
+
+        print(number, k, result)
