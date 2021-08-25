8a9938bd9d0644107d323323c8d1d903ec059555
swkim0128 <swkim0128@gmail.com>
2021-08-26 08:53:45 +0900

    DOC: modify main, input.txt
    알고리즘 수행에 있어 수정이 필요한 main, input.txt

--- java/src/algorithm/Main.java
+++ java/src/algorithm/Main.java
@@ -1,11 +1,12 @@
 package algorithm;
 
 import algorithm.baekjoon.*;
+import algorithm.jungol.jungol_1828;
 import algorithm.swexpert.*;
 
 public class Main {
     Main() {
-        Excute.excute(new baek_16935());
+        Excute.excute(new baek_10026());
     }
 
     public static void main(String[] args) {
