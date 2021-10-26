c2a602d648488165b058a5023f7bb410abb884e0
김성우 <swkim0128@naver.com>
2021-10-26 10:37:58 +0900

    ALGO: modify

--- java/src/algorithm/Main.java
+++ java/src/algorithm/Main.java
@@ -9,7 +9,7 @@ import algorithm.swexpert.*;
 
 public class Main {
     Main() {
-        Excute.excute(new baek_16137());
+        Excute.excute(new baek_16987());
     }
 
     public static void main(String[] args) {
