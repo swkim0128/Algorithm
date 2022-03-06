75defb50e706c9b762512dfe00eaee43097878af
김성우 <swkim0128@naver.com>
2022-03-06 14:48:18 +0900

    DOC : modify file
    리드미 파일 내용 수정 및 자바 파일 수정

---
+++ java/src/algorithm/baekjoon/baek_1786.java
@@ -0,0 +1,16 @@
+package algorithm.baekjoon;
+
+import algorithm.Solution;
+
+import java.io.IOException;
+
+public class baek_1786 extends Solution {
+
+    @Override
+    public void solution() throws IOException {
+        String T = read.readLine();
+        String P = read.readLine();
+
+
+    }
+}
